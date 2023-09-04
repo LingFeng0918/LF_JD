52c2d6b42a49f7e17fc77b7497fbd8b6e034f9c1
LingFeng0918 <39041664+LingFeng0918@users.noreply.github.com>
2023-09-04 14:57:43 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -98,6 +98,5 @@ let envName = process.env.ShareCodeEnvName
 if (nameChinese && nameConfig && envName) {
   SetShareCodesEnv(nameChinese, nameConfig, envName)
 } else {
-    console.debug(`KingRan 频道通知：https://t.me/KingRan521\n`)
-		console.debug(`云服务器IP须知：域名前缀为 'lzdz' 的禁用勿跑容易黑号\n`)
+   console.debug(`云服务器IP须知：域名前缀为 'lzdz' 的禁用勿跑容易黑号\n`)
 }
